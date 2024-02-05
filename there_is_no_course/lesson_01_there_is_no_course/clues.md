a733a3dae6f1ff970289ca757944af9f55028178
Alistair Miles <alimanfoo@googlemail.com>
2024-02-05 15:17:54 +0000

    refactor

---
+++ there_is_no_course/lesson_01_there_is_no_course/clues.md
@@ -0,0 +1,15 @@
+I'm not really working.
+
+I'm just pretending to work.
+
+There is a course really.
+
+Well, sort of.
+
+I will leave you clues and set you challenges.
+
+If you solve them, you might find the next lesson.
+
+Good luck!
+
+(You're gonna need it.)
---
+++ there_is_no_course/lessons.py
@@ -0,0 +1,28 @@
+"""
+This script contains a complete course in how to become
+an awesome Python programmer.
+
+Copyright @alimanfoo. All rights reserved.
+"""
+
+
+def lesson_01_there_is_no_course():
+    raise NotImplementedError(
+        """
+        There is no training course. 
+        Programming is magic. 
+        You either have it, or you don't. 
+        Sorry.
+    """
+    )
+
+
+def lesson_02_the_big_red_button():
+    raise NotImplementedError(
+        """
+        There is no training course. 
+        Programming is magic. 
+        You either have it, or you don't. 
+        Sorry.
+    """
+    )
