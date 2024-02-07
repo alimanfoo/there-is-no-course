c31abcf7d02a2567b6a2382610e00b2a6e76f07a
Alistair Miles <alimanfoo@googlemail.com>
2024-02-07 01:32:22 +0000

    fix play

--- there_is_no_course/lesson_03_the_bridge_of_death/the_bridge_of_death.py
+++ there_is_no_course/lesson_03_the_bridge_of_death/the_bridge_of_death.py
@@ -14,29 +14,41 @@ def render():
     # easter egg question which the player can find by hacking
     # the URL.
     question = st.query_params.get("question", "intro")
+
+    # This is a hack, to make sure that the query parameters are
+    # visible in the URL. This isn't necessary when running streamlit
+    # locally, but is necessary when running on streamlit community
+    # cloud, because the app is being run within an iframe. Fun eh?
     st.query_params["question"] = question
+
+    # Deal with different rendering scenarios, based on value of
+    # question parameter.
     if question == "intro":
         render_intro()
     elif question == "1":
         render_question_one()
     elif question == "2":
         render_question_two()
-    elif question == "4":
-        render_question_four()
     # This is the easter egg option, can only be found by URL hacking.
     elif question == "3":
         render_question_three()
+    elif question == "4":
+        render_question_four()
     elif question == "offyougo":
         render_success()
     else:
-        render_bad_question()
+        render_bad_query_param()
 
 
 def init_session():
     session.setdefault("question_one_tries", 0)
+    session.setdefault("question_one_answer", "")
     session.setdefault("question_two_tries", 0)
+    session.setdefault("question_two_answer", "")
     session.setdefault("question_three_tries", 0)
+    session.setdefault("question_three_answer", "")
     session.setdefault("question_four_tries", 0)
+    session.setdefault("question_four_answer", "")
 
 
 def render_header():
@@ -68,8 +80,38 @@ def render_intro():
     )
 
 
+def render_correct(next, text):
+    st.markdown(
+        f"""
+        **Correct!** <a href="?question={next}" rel="next" target="_self">{text} &gt;&gt;&gt;</a>
+    """,
+        unsafe_allow_html=True,
+    )
+
+
+def render_death():
+    st.markdown("**Auuuuuuuugh!**")
+    st.markdown("(You have been cast into the Gorge of Eternal Peril.)")
+
+
+def submit_question_one():
+    session.question_one_tries += 1
+
+
+def submit_question_two():
+    session.question_two_tries += 1
+
+
+def submit_question_three():
+    session.question_three_tries += 1
+
+
+def submit_question_four():
+    session.question_four_tries += 1
+
+
 def render_question_one():
-    tries = session.question_one_tries
+    # Set up question.
     hints = [
         "Sorry, try again.",
         "Still not right. Another try maybe?",
@@ -82,33 +124,42 @@ def render_question_one():
         "Last try...",
     ]
     correct_answers = ["guido van rossum"]
-    if tries < len(hints):
-        with st.form(key="question_one_form"):
-            st.markdown(
-                """
-                **Question 1:** Who invented the Python programming language?
-            """
-            )
-            answer = st.text_input(label="``>>>``")
-            submitted = st.form_submit_button(label="Submit")
 
-        if submitted:
-            if answer.lower() in correct_answers:
-                st.markdown(
-                    """
-                    Correct! <a href="?question=2" rel="next" target="_self">Next question &gt;&gt;&gt;</a>
-                """,
-                    unsafe_allow_html=True,
-                )
-            else:
-                st.markdown(hints[tries])
-                session.question_one_tries += 1
-    else:
+    # Obtain session variables.
+    tries = session.question_one_tries
+    answer = session.question_one_answer.lower()
+    correct = answer in correct_answers
+
+    # Render initial content.
+    st.markdown(
+        f"""
+        ## Question 1
+
+        **Who invented the Python programming language?**
+
+        Tries: {tries}/{len(hints) + 1}
+    """
+    )
+
+    if correct:
+        # Answer is correct, render link to next question.
+        render_correct(next=2, text="Next question")
+        return
+
+    if tries > len(hints):
+        # Answer is not correct, and there are no more hints.
         render_death()
+        return
+
+    with st.form(key="question_one_form"):
+        st.text_input(label="``>>>``", key="question_one_answer")
+        st.form_submit_button(label="Submit", on_click=submit_question_one)
+        if tries > 0:
+            st.markdown(hints[tries - 1])
 
 
 def render_question_two():
-    tries = session.question_two_tries
+    # Set up question.
     hints = [
         "Incorrect.",
         "Also incorrect.",
@@ -126,35 +177,112 @@ def render_question_two():
         "monty pythons flying circus",
         "monty python's flying circus",
     ]
-    if tries < len(hints):
-        with st.form(key="question_two_form"):
-            st.markdown(
-                """
-                **Question 2**: What famous British comedy TV show is the 
-                Python programming language named after?
-            """
-            )
-            answer = st.text_input(label="``>>>``")
-            submitted = st.form_submit_button(label="Submit")
 
-        if submitted:
-            if answer.lower() in correct_answers:
-                # Deliberately skip over question 3, that is the easter egg.
-                st.markdown(
-                    """
-                    Correct! <a href="?question=4" rel="next" target="_self">Next question &gt;&gt;&gt;</a>
-                """,
-                    unsafe_allow_html=True,
-                )
-            else:
-                st.markdown(hints[tries])
-                session.question_two_tries += 1
-    else:
+    # Obtain session variables.
+    tries = session.question_two_tries
+    answer = session.question_two_answer.lower()
+    correct = answer in correct_answers
+
+    # Render initial content.
+    st.markdown(
+        f"""
+        ## Question 2
+
+        **What famous British comedy TV show is the Python 
+        programming language named after?**
+
+        Tries: {tries}/{len(hints) + 1}
+    """
+    )
+
+    if correct:
+        # Answer is correct, render link to next question.
+        # N.B., deliberately skip question 3 here.
+        render_correct(next=4, text="Next question")
+        return
+
+    if tries > len(hints):
+        # Answer is not correct, and there are no more hints.
         render_death()
+        return
+
+    with st.form(key="question_two_form"):
+        st.text_input(label="``>>>``", key="question_two_answer")
+        st.form_submit_button(label="Submit", on_click=submit_question_two)
+        if tries > 0:
+            st.markdown(hints[tries - 1])
+
+
+def render_question_three():
+    # Set up question.
+    hints = [
+        "Did you watch the video?",
+        "It's funny, isn't it.",
+        "Especially the bit at the end.",
+        "When King Arthur is answering the questions.",
+        "His answer to the third question is very clever.",
+        "Don't you think?",
+        "The bridgekeeper didn't see that coming!",
+        "Worth watching again, I'd say.",
+        "Oh well, I gave you lots of chances...",
+    ]
+    correct_answers = [
+        "What do you mean? An African or European swallow?".lower(),
+        "An African or European swallow?".lower(),
+        "African or European swallow?".lower(),
+        "African or European?".lower(),
+    ]
+
+    # Obtain session variables.
+    tries = session.question_three_tries
+    answer = session.question_three_answer.lower()
+    # Be kind to the player, don't require a question mark.
+    if answer and answer[-1] != "?":
+        answer += "?"
+    correct = answer in correct_answers
+
+    # Render initial content.
+    st.markdown(
+        f"""
+        ## Question 3
+
+        **What is the air-speed velocity of an unladen swallow?**
+
+        Tries: {tries}/{len(hints) + 1}
+    """
+    )
+
+    if correct:
+        # Answer is correct, render link to success.
+        st.markdown(
+            """
+            Huh? I-- I don't know that. 
+            
+            **Auuuuuuuugh!**
+
+            (Well done! You have cast the bridgekeeper into the Gorge of Eternal Peril.)
+            
+            <a href="?question=offyougo" rel="next" target="_self">Off you go &gt;&gt;&gt;</a>
+        """,
+            unsafe_allow_html=True,
+        )
+        return
+
+    if tries > len(hints):
+        # Answer is not correct, and there are no more hints.
+        render_death()
+        return
+
+    st.video(data="https://youtu.be/0D7hFHfLEyk")
+    with st.form(key="question_three_form"):
+        st.text_input(label="``>>>``", key="question_three_answer")
+        st.form_submit_button(label="Submit", on_click=submit_question_three)
+        if tries > 0:
+            st.markdown(hints[tries - 1])
 
 
 def render_question_four():
-    tries = session.question_four_tries
+    # Set up question.
     hints = [
         "Nope.",
         "Really?",
@@ -206,96 +334,46 @@ def render_question_four():
         "https://jython.org/",
         "https://www.jython.org/",
     ]
-    if tries < len(hints):
-        with st.form(key="question_four_form"):
-            st.markdown(
-                """
-                **Question 4**: What website would you go to if you 
-                wanted to download and install Python on your
-                computer?
-            """
-            )
-            answer = st.text_input(label="``>>>``")
-            submitted = st.form_submit_button(label="Submit")
 
-        if submitted:
-            if answer.lower() in correct_answers:
-                st.markdown(
-                    """
-                    Correct! <a href="?question=offyougo" rel="next" target="_self">Off you go &gt;&gt;&gt;</a>
-                """,
-                    unsafe_allow_html=True,
-                )
-            elif answer.lower() in smart_answers:
-                # This is a bit of fun, attempt to catch out any
-                # smartypants people out there who already know a
-                # lot about Python.
-                raise SmartyPantsError
-            else:
-                st.markdown(hints[tries])
-                session.question_four_tries += 1
-    else:
+    # Obtain session variables.
+    tries = session.question_one_tries
+    answer = session.question_one_answer.lower()
+    correct = answer in correct_answers
+    smart = answer in smart_answers
+
+    # Render initial content.
+    st.markdown(
+        f"""
+        ## Question 4
+
+        **What website would you go to if you wanted to download 
+        and install Python on your computer?**
+
+        Tries: {tries}/{len(hints) + 1}
+    """
+    )
+
+    if correct:
+        # Answer is correct, render link to next question.
+        render_correct(next="offyougo", text="Off you go")
+        return
+
+    if smart:
+        # This is a bit of fun, attempt to catch out any
+        # smartypants people out there who already know a
+        # lot about Python.
+        raise SmartyPantsError
+
+    if tries > len(hints):
+        # Answer is not correct, and there are no more hints.
         render_death()
+        return
 
-
-def render_question_three():
-    tries = session.question_three_tries
-    hints = [
-        "Did you watch the video?",
-        "It's funny, isn't it.",
-        "Especially the bit at the end.",
-        "When King Arthur is answering the questions.",
-        "His answer to the third question is very clever.",
-        "Don't you think?",
-        "The bridgekeeper didn't see that coming!",
-        "Worth watching again, I'd say.",
-        "Oh well, I gave you lots of chances...",
-    ]
-    correct_answers = [
-        "What do you mean? An African or European swallow?".lower(),
-        "An African or European swallow?".lower(),
-        "African or European swallow?".lower(),
-        "African or European?".lower(),
-    ]
-    if tries < len(hints):
-        st.video(data="https://youtu.be/0D7hFHfLEyk")
-        with st.form(key="question_three_form"):
-            st.markdown(
-                """
-                **Question 3**: What is the air-speed velocity of an unladen swallow?
-            """
-            )
-            answer = st.text_input(label="``>>>``")
-            submitted = st.form_submit_button(label="Submit")
-
-        if submitted:
-            answer = answer.lower()
-            # Be kind to the player, don't require a question mark.
-            if answer and answer[-1] != "?":
-                answer += "?"
-            if answer in correct_answers:
-                st.markdown(
-                    """
-                    Huh? I-- I don't know that. 
-                    
-                    ## Auuuuuuuugh!
-
-                    (Well done! You have cast the bridgekeeper into the Gorge of Eternal Peril.)
-                    
-                    <a href="?question=offyougo" rel="next" target="_self">Off you go &gt;&gt;&gt;</a>
-                """,
-                    unsafe_allow_html=True,
-                )
-            else:
-                st.markdown(hints[tries])
-                session.question_three_tries += 1
-    else:
-        render_death()
-
-
-def render_death():
-    st.markdown("## Auuuuuuuugh!")
-    st.markdown("(You have been cast into the Gorge of Eternal Peril.)")
+    with st.form(key="question_one_form"):
+        st.text_input(label="``>>>``", key="question_one_answer")
+        st.form_submit_button(label="Submit", on_click=submit_question_one)
+        if tries > 0:
+            st.markdown(hints[tries - 1])
 
 
 def render_success():
@@ -309,7 +387,7 @@ def render_success():
     )
 
 
-def render_bad_question():
+def render_bad_query_param():
     st.markdown(
         """
         What do you think you are doing, hacking around with my URL 
@@ -325,4 +403,6 @@ class SmartyPantsError(Exception):
 
 
 if __name__ == "__main__":
+    # If we are running this as a script (as would be expected
+    # if running via streamlit) then render the UI.
     render()
